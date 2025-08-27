25767c519723248210d5212fccae3d164c03394a
David Sagan <dcs16@cornell.edu>
2025-08-26 21:12:54 -0400

    Minor tweaks to aperture and alignment.

--- src/aperture.jl
+++ src/aperture.jl
@@ -2,15 +2,17 @@
 @enumx ApertureAt::UInt8 Entrance Exit BothEnds
 
 @kwdef mutable struct ApertureParams{T} <: AbstractParams
-  x1_limit::T                     = Float32(0)                     
-  x2_limit::T                     = Float32(0)
-  y1_limit::T                     = Float32(0)
-  y2_limit::T                     = Float32(0) 
+  x1_limit::T                     = -Inf32
+  x2_limit::T                     =  Inf32
+  y1_limit::T                     = -Inf32
+  y2_limit::T                     =  Inf32
   aperture_shape::ApertureShape.T = ApertureShape.Elliptical
   aperture_at::ApertureAt.T       = ApertureAt.Entrance
   aperture_shifts_with_body::Bool = true
-  function ApertureParams(x1_limit, x2_limit, y1_limit, y2_limit, aperture_shape, aperture_at, aperture_shifts_with_body)
-    return new{promote_type(typeof(x1_limit),typeof(x2_limit),typeof(y1_limit),typeof(y2_limit))}(x1_limit, x2_limit, y1_limit, y2_limit, aperture_shape, aperture_at, aperture_shifts_with_body)
+  aperture_active::Bool           = true
+
+  function ApertureParams(x1_limit, x2_limit, y1_limit, y2_limit, aperture_shape, aperture_at, aperture_shifts_with_body, aperture_active)
+    return new{promote_type(typeof(x1_limit),typeof(x2_limit),typeof(y1_limit),typeof(y2_limit))}(x1_limit, x2_limit, y1_limit, y2_limit, aperture_shape, aperture_at, aperture_shifts_with_body, aperture_active)
   end
 end
 
@@ -24,9 +26,12 @@ function Base.isapprox(a::ApertureParams, b::ApertureParams)
          a.y2_limit                  ≈  b.y2_limit        &&
          a.aperture_shape            ==  b.aperture_shape &&
          a.aperture_at               ==  b.aperture_at    &&
-         a.aperture_shifts_with_body ==  b.aperture_shifts_with_body
+         a.aperture_shifts_with_body ==  b.aperture_shifts_with_body &&
+         a.aperture_active           ==  b.aperture_active
 end
-#Base.getproperty(a::ApertureParams, key::Symbol) = deval(getfield(a, key))
+
+# Base.getproperty(a::ApertureParams, key::Symbol) = deval(getfield(a, key))
+
 function deval(a::ApertureParams{<:DefExpr})
   return ApertureParams(
     deval(a.x1_limit),
@@ -35,11 +40,20 @@ function deval(a::ApertureParams{<:DefExpr})
     deval(a.y2_limit),
     deval(a.aperture_shape),
     deval(a.aperture_at),
-    deval(a.aperture_shifts_with_body),   
+    deval(a.aperture_shifts_with_body),
+    deval(a.aperture_active),
   )
 end
 
+#
 
-# Note that here the reference energy is really needed to compute anything
-# other than the above so there is no more work to do here. Must define 
-# virtual properties for the rest of them.
+function isactive(ap::ApertureParams)
+  if !ap.aperture_active; return false; end
+
+  if ap.aperture_shape == ApertureShape.Elliptical &&
+          (ap.x1_limit == -Inf || ap.x2_limit == Inf || ap.y1_limit == -Inf || ap.y2_limit == Inf)
+    return false
+  end
+
+  return true
+end
