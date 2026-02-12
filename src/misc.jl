3803269608e6d57b9f78f76c5dcebdde6778d40e
mattsignorelli <mgs255@cornell.edu>
2026-02-12 14:51:57 -0500

    clean and add cov

--- src/misc.jl
+++ src/misc.jl
@@ -8,13 +8,10 @@ function Base.isapprox(a::MapParams, b::MapParams)
          all(a.transport_map_params .≈ b.transport_map_params)
 end
 
-# === THIS BLOCK WAS WRITTEN BY CLAUDE ===
+# === THIS BLOCK WAS PARTIALLY WRITTEN BY CLAUDE ===
 # Generated function for arbitrary-length tuples
 @generated function deval(mp::MapParams{F,P}) where {F,P}
     N = length(P.parameters)
-    if N == 0
-        return :(())
-    end
     # Use getfield with literal integer arguments
     exprs = [:(deval(Base.getfield(mp.transport_map_params, $i))) for i in 1:N]
     return :(MapParams(mp.transport_map, tuple($(exprs...))))
